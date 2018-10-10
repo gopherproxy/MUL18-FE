042fa8d1d523fd59bf949026884b1b6a2f199bd7
gopherproxy <gopherproxy@gmail.com>
2018-10-10 15:21:29 +0200

    Angular 6 app - Components, routing, navigation

--- angular6app/src/app/app.module.ts
+++ angular6app/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+
+const appRoutes: Routes = [
+ // sets home page (base URL)
+  { path: 'home', component: HomeComponent },
+// redirects to home
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
