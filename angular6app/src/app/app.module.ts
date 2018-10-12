d4985374332ec5fdcc63e4094c5351181f813162
gopherproxy <gopherproxy@gmail.com>
2018-10-12 16:24:40 +0200

    Angular 6 app final

--- angular6app/src/app/app.module.ts
+++ angular6app/src/app/app.module.ts
@@ -26,7 +26,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-	RouterModule.forRoot(appRoutes),
+	RouterModule.forRoot(appRoutes, { useHash: true }),
     HttpClientModule
   ],
   providers: [],
